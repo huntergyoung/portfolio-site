46fd280b648b221962b298e944ee903fbcbe6e9a
Hunter Young <huntergyoung@gmail.com>
2016-05-13 22:34:57 -0400

    Favicon and shiz

--- js/custom.js
+++ js/custom.js
@@ -34,14 +34,31 @@ $('.row .btn').on('click', function(e) {
     $collapse.collapse('toggle');
 });
 
-// Box fade //
 
-var $imgs = $(".fadeInImage").hide();
 
-function fadeOneByOne(i){
-   if(i >= $imgs.size()) return;
-   $imgs.eq(i).fadeIn(function(){ fadeOneByOne(i+1); })
-}
+$(function() {
+    var Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
 
-fadeOneByOne(0);
+        // Variables privadas
+        var links = this.el.find('.link');
+        // Evento
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    }
 
+    Accordion.prototype.dropdown = function(e) {
+        var $el = e.data.el;
+            $this = $(this),
+            $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        };
+    }   
+
+    var accordion = new Accordion($('#accordion'), false);
+});
